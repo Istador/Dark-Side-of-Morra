0a58074aa12e21b9a27429bc31d73bd2f34fd243
Robin C. Ladiges <rcl.git@blackpinguin.de>
2013-08-21 02:11:35 +0000

    SpriteController randomisiert, damit nicht alle Objekte zum selben Zeitpunkt (Time.time) beim selben Animationsschritt (index) sind.

--- Assets/Scripts/SpriteController.cs
+++ Assets/Scripts/SpriteController.cs
@@ -3,21 +3,38 @@ using System.Collections;
 
 public class SpriteController : MonoBehaviour
 {
+	private int rndCol;
+	
+	
+	SpriteController(){
+		//random positive integer -> random animation position for different entities
+		rndCol = System.Math.Abs(new System.Random().Next());
+	}
+	
 	public void  animate (int columnSize, int rowSize, int colFrameStart, int rowFrameStart, int totalFrames, int framesPerSecond)
 	{
+		/*
+		 * TODO
+		 * 
+		 * Ist columnSize nicht eigtl. das selbe wie totalFrames ?
+		 * 
+		 * Ist colFrameStart nicht überflüssig weil es immer 0 ist ?
+		 */
+		
 		// time control fps
 		int index = (int) (Time.time * framesPerSecond);
 		// modulate
-		index = index % totalFrames;
+		index %= totalFrames;
 
 		// scale
 		Vector2 size = new Vector2( (1.0f / columnSize) , (1.0f / rowSize) );
 		// offset
-		float u = index % columnSize;
-		float v = index / columnSize;
-		//Debug.Log(u + " " + v);
-
-		Vector2 offset = new Vector2( (u + colFrameStart) * size.x, (1.0f - size.y) - ( (v + rowFrameStart) * size.y) );
+		float u = index + rndCol % columnSize;
+		
+		//float v = index / columnSize; //v is always 0 because both are int and index < columnSize
+		//it also makes no sense to animate on the X axis AND on the Y axis simultaneously
+				
+		Vector2 offset = new Vector2( (u + colFrameStart) * size.x, (1.0f - size.y) - ( (rowFrameStart) * size.y) );
 		
 		// texture scale
 		renderer.material.mainTextureScale = size;
